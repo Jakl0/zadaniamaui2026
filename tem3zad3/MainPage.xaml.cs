3bf4bf251d05b550b35b190c95ab3b04f66a73a0
5M <5M@415-16>
2026-09-21 12:17:43 +0200

    dodanie zadan

---
+++ tem3zad3/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace tem3zad3
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OrderButtonClicked(object sender, EventArgs e)
+        {
+
+            if (!string.IsNullOrWhiteSpace(ProductEntry.Text)&& !string.IsNullOrWhiteSpace(QuantityEntry.Text)&& !string.IsNullOrWhiteSpace(AdressEntry.Text))
+            {
+
+                DisplayAlert("Hej","Co jest","OK");
+
+            }
+
+        }
+    }
+}
---
+++ tem3zad3/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace tem3zad3
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
